60d24e2407b01b53f625bb00289d45e7726cb814
Nikolay Izyumov <nikolay.izyumov@yandex.ru>
2021-09-16 13:46:29 +0300

    add opt parser version with default value

--- include/pcomb/optional.h
+++ include/pcomb/optional.h
@@ -1,8 +1,10 @@
 #ifndef PCOMB_OPTIONAL_H_
 #define PCOMB_OPTIONAL_H_
 
+#include <functional>
 #include <utility>
 
+#include "pcomb/adaptive.h"
 #include "pcomb/privates/optional.h"
 
 namespace pcomb {
@@ -11,6 +13,19 @@ template <typename P>
 inline auto Opt(P&& parser) {
   return privates::OptionalParser<std::remove_reference_t<P>>(
       std::forward<P>(parser));
+}
+
+template <typename P, typename T>
+inline auto ParseOrDefault(P&& parser, const T& default_value) {
+  auto maybe = Opt(std::forward<P>(parser));
+
+  using R = typename decltype(maybe)::ValueType;
+  using V = typename R::value_type;
+  std::function<V(R)> adapter =
+      [=](R&& opt) {
+        return opt.value_or(default_value);
+      };
+  return Adapted(std::move(maybe), std::move(adapter));
 }
 
 }  // namespace pcomb
--- test/optional_test.cc
+++ test/optional_test.cc
@@ -7,6 +7,7 @@
 
 using pcomb::Char;
 using pcomb::Opt;
+using pcomb::ParseOrDefault;
 
 class OptionalParserTest : public ::testing::Test { };
 
@@ -26,4 +27,22 @@ TEST_F(OptionalParserTest, HeadNotMatch) {
   auto parser = Opt(Char('A'));
   using Expected = decltype(parser)::ValueType;
   TestParserSuccess("BA", parser, Expected{}, 0, CheckNotEmpty('B'));
+}
+
+TEST_F(OptionalParserTest, EmptyWithDefault) {
+  auto parser = ParseOrDefault(Char('A'), 'B');
+  using Expected = decltype(parser)::ValueType;
+  TestParserSuccess("", parser, Expected{'B'}, 0, CheckEmpty());
+}
+
+TEST_F(OptionalParserTest, HeadMatchWithDefault) {
+  auto parser = ParseOrDefault(Char('A'), 'B');
+  using Expected = decltype(parser)::ValueType;
+  TestParserSuccess("AB", parser, Expected{'A'}, 1, CheckNotEmpty('B'));
+}
+
+TEST_F(OptionalParserTest, HeadNotMatchWithDefault) {
+  auto parser = ParseOrDefault(Char('A'), 'B');
+  using Expected = decltype(parser)::ValueType;
+  TestParserSuccess("BA", parser, Expected{'B'}, 0, CheckNotEmpty('B'));
 }

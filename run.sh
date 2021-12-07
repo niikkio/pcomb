dd4c3c22b5cc8a2d3e0fd6a5e1e0f828988f97e2
Nikolay Izyumov <nikolay.izyumov@yandex.ru>
2021-12-08 02:00:45 +0300

    now generate test coverage report

--- run.sh
+++ run.sh
@@ -7,3 +7,12 @@ if [ -n "$1" ]; then
 fi
 
 cd build && eval "$ctest_command"
+
+COVERAGE_WORKING_PATH=test/CMakeFiles/core_test.dir
+COVERAGE_INFO_FILENAME=${COVERAGE_WORKING_PATH}/cov.info
+FILTERED_COVERAGE_INFO_FILENAME=${COVERAGE_WORKING_PATH}/filtered_cov.info
+
+lcov -o ${COVERAGE_INFO_FILENAME} -c -d ${COVERAGE_WORKING_PATH}
+lcov --remove ${COVERAGE_INFO_FILENAME} '*/test/*' '*/build/_deps/*' '/usr/include/*' -o ${FILTERED_COVERAGE_INFO_FILENAME}
+genhtml --demangle-cpp -o report ${FILTERED_COVERAGE_INFO_FILENAME}
+lcov --zerocounters -d ${COVERAGE_WORKING_PATH}
